a3c6981c9bb6f48a1215cc2af00806bc77673f8f
Prince Le Bon <jeffersonhoundjo@gmail.com>
2022-07-03 21:49:27 +0100

    Ajout des fichiers

---
+++ datatypes.js
@@ -0,0 +1,9 @@
+console.log(typeof prenom)
+console.log(typeof trouve)
+console.log(typeof birth)
+console.log(typeof none)
+/*console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined*/
